63f76deac89acf31ddaac653563c30999a746801
Italo Lon kan <sanchotorres777@gmail.com>
2023-05-28 01:19:00 -0500

    Update config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    DB_URI: "mongodb+srv://demo:2ZS91XrwfQqNbFyU@cluster0.8kaczfr.mongodb.net/?retryWrites=true&w=majority",
+    NEXTAUTH_SECRET: "123456"
+  }
+}
 
 module.exports = nextConfig
